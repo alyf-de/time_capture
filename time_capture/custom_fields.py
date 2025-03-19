feaef4860ab5ac4baab22b62740974341760fa88
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2025-03-19 15:51:29 +0100

    chore: create/update Custom Fields and Property Setters (DRC-134)

--- time_capture/custom_fields.py
+++ time_capture/custom_fields.py
@@ -1,89 +1,52 @@
+from .utils import identity as _
 
-custom_fields = {
-	"Attendance": [
-		{
-			"fieldname": "expected_working_hours",
-			"fieldtype": "Float",
-			"insert_after": "working_hours",
-			"label": "Expected Working Hours",
-			"read_only": 1,
-			"default": "0",
-			"translatable": 0,
-		},
-		{
-			"label": "Flexitime",
-			"fieldname": "flexitime",
-			"insert_after": "expected_working_hours",
-			"fieldtype": "Float",
-			"read_only": 1,
-			"default": "0",
-		},
-		{
-			"label": "Time Capture",
-			"fieldname": "custom_time_capture",
-			"insert_after": "attendance_request",
-			"fieldtype": "Link",
-			"options": "Time Capture",
-			"read_only": 1,
-		},
-	],
-	"Employee": [
-		{
-			"fieldname": "expected_daily_working_hours",
-			"fieldtype": "Float",
-			"insert_after": "attendance_device_id",
-			"label": "Expected Daily Working Hours",
-			"translatable": 0,
-		},
-	],
-	"Timesheet": [
-		{
-			"label": "Time Capture",
-			"fieldname": "custom_time_capture",
-			"insert_after": "parent_project",
-			"fieldtype": "Link",
-			"options": "Time Capture",
-			"read_only": 1,
-		},
-	],
-}
 
-not_sure_if_needed = {
-	"Task": [
-		{
-			"fieldname": "naming_series",
-			"label": "Naming Series",
-			"insert_after": "column_break0",
-			"fieldtype": "Select",
-			"reqd": 0,
-			"hidden": 1,
-			"depends_on": "eval: doc.__islocal",
-		},
-		{
-			"fieldname": "custom_is_active",
-			"label": "Is Active",
-			"insert_after": "subject",
-			"fieldtype": "Select",
-			"options": "Yes\nNo",
-			"default": "No",
-			"translatable": 1,
-		},
-		{
-			"fieldname": "custom_project_manager",
-			"label": "Project Manager",
-			"insert_after": "project",
-			"fieldtype": "Link",
-			"options": "User",
-			"fetch_from": "project.custom_project_manager",
-			"read_only": 1,
-		},
-		{
-			"fieldname": "custom_hourly_billed",
-			"label": "Hourly Billed",
-			"insert_after": "color",
-			"fieldtype": "Check",
-			"default": "1",
-			"depends_on": "eval: !doc.__islocal",
-		},
-	],
-}
+def get_custom_fields():
+	return {
+		"Attendance": [
+			{
+				"fieldname": "expected_working_hours",
+				"fieldtype": "Float",
+				"insert_after": "working_hours",
+				"label": "Expected Working Hours",
+				"read_only": 1,
+				"default": "0",
+				"translatable": 0,
+			},
+			{
+				"label": "Flexitime",
+				"fieldname": "flexitime",
+				"insert_after": "expected_working_hours",
+				"fieldtype": "Float",
+				"read_only": 1,
+				"default": "0",
+			},
+			{
+				"label": "Time Capture",
+				"fieldname": "custom_time_capture",
+				"insert_after": "attendance_request",
+				"fieldtype": "Link",
+				"options": "Time Capture",
+				"read_only": 1,
+			},
+		],
+		"Employee": [
+			{
+				"fieldname": "expected_daily_working_hours",
+				"fieldtype": "Float",
+				"insert_after": "attendance_device_id",
+				"label": "Expected Daily Working Hours",
+				"translatable": 0,
+			},
+		],
+		"Timesheet": [
+			{
+				"label": "Time Capture",
+				"fieldname": "custom_time_capture",
+				"insert_after": "parent_project",
+				"fieldtype": "Link",
+				"options": "Time Capture",
+				"read_only": 1,
+			},
+		],
+	}
--- time_capture/property_setters.py
+++ time_capture/property_setters.py
@@ -1,3 +1,6 @@
-property_setters = [
-	("Employee", "working_hours_per_week", "hidden", "1"), # TODO: What is more important? Week or day?
-]
+
+
+def get_property_setters():
+	return [
+		("Employee", "working_hours_per_week", "hidden", "1"), # TODO: What is more important? Week or day?
+	]
