92cf6359e96a766444841765087f28bb3eae15a9
Marc-Constantin Enke <marc@alyf.de>
2025-05-01 09:29:18 +0200

    feat: create leave application when approved

--- time_capture/time_capture/doctype/bulk_leave_application/bulk_leave_application.py
+++ time_capture/time_capture/doctype/bulk_leave_application/bulk_leave_application.py
@@ -3,11 +3,16 @@
 
 import frappe
 from frappe.model.document import Document
+from hrms.hr.utils import share_doc_with_approver, validate_active_employee
 
 
 class BulkLeaveApplication(Document):
-	def on_submit(self):
-		self.create_leave_applications()
+	def on_update_after_submit(self):
+		if self.status == "Approved":
+			self.create_leave_applications()
+
+	def on_update(self):
+		share_doc_with_approver(self, self.leave_approver)
 
 	def create_leave_applications(self):
 		for item in self.table_leaves:
@@ -19,5 +24,7 @@ class BulkLeaveApplication(Document):
 			leave_application.status = self.status
 			leave_application.from_date = item.from_date
 			leave_application.to_date = item.to_date
-			leave_application.reason = item.description
+			leave_application.reason = item.reason
+			leave_application.follow_via_email = 0
 			leave_application.insert()
+			leave_application.submit()
