cd10aaffaf1573ba20028bd264e66808597f5ec8
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2025-05-12 08:44:54 +0200

    feat: set Leave Approver and limit options to valid approvers only

--- time_capture/time_capture/doctype/bulk_leave_application/bulk_leave_application.js
+++ time_capture/time_capture/doctype/bulk_leave_application/bulk_leave_application.js
@@ -8,16 +8,50 @@
 // });
 
 frappe.ui.form.on("Bulk Leave Application", {
+	setup: function (frm) {
+		frm.set_query("leave_approver", function () {
+			return {
+				query: "hrms.hr.doctype.department_approver.department_approver.get_approvers",
+				filters: {
+					employee: frm.doc.employee,
+					doctype: "Leave Application",
+				},
+			};
+		});
+		frm.set_query("employee", erpnext.queries.employee);
+	},
+
 	from_date: function (frm) {
 		if (frm.doc.from_date && !frm.doc.to_date) {
 			var a_year_from_start = frappe.datetime.add_months(frm.doc.from_date, 12);
 			frm.set_value("to_date", frappe.datetime.add_days(a_year_from_start, -1));
 		}
 	},
-});
 
-frappe.ui.form.on("Bulk Leave Application Date", {
-	from_date: function (frm, cdt, cdn) {
+	employee: function (frm) {
+		frm.trigger("set_leave_approver");
+	},
+
+	set_leave_approver: function (frm) {
+		if (frm.doc.employee) {
+			return frappe.call({
+				method: "hrms.hr.doctype.leave_application.leave_application.get_leave_approver",
+				args: {
+					employee: frm.doc.employee,
+				},
+				callback: function (r) {
+					if (r && r.message) {
+						frm.set_value("leave_approver", r.message);
+					}
+				},
+			});
+		}
+	},
+});
+	
+	frappe.ui.form.on("Bulk Leave Application Date", {
+		from_date: function (frm, cdt, cdn) {
 		frappe.model.set_value(cdt, cdn, "to_date", locals[cdt][cdn].from_date);
 	},
 });
+
