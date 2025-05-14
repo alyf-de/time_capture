1ec0fc9efb3966d0ce5cbfe7d476ddcb6cde2d9f
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2025-05-14 16:46:15 +0200

    feat: add utility function get_expected_working_hours

--- time_capture/scripts/employee.py
+++ time_capture/scripts/employee.py
@@ -9,3 +9,15 @@ def before_validate(doc, method):
 def validate_expected_working_hours(doc):
 	if not doc.date_of_joining == min(ewh.valid_from for ewh in doc.expected_working_hours):
 		frappe.throw(_("Date of Joining is not the same as the earliest date of Expected Working Hours."))
+
+
+def get_expected_working_hours(employee_id, date):
+	"""
+	Get the expected working hours for an employee on a specific date.
+	"""
+	return frappe.db.get_value(
+		"Employee Expected Working Hours",
+		filters={"parent": employee_id, "valid_from": ["<=", date]},
+		fieldname="expected_daily_working_hours",
+		order_by="valid_from desc",
+	)
