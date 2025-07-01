6383109b99c32f1d05b072bd73d1fcad947d0865
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2025-07-01 14:24:17 +0200

    fix: remove typos and further refactor

--- time_capture/patches/move_expected_working_time_to_child_table.py
+++ time_capture/patches/move_expected_working_time_to_child_table.py
@@ -2,12 +2,17 @@ import frappe
 
 
 def execute():
-	if not frappe.db.exists(
-		"Custom Field", {"dt": "Employee", "fieldname": "expected_daily_working_hours", "fieldtype": "Float"}
-	):
+	custom_field_filters = {
+		"dt": "Employee",
+		"fieldname": "expected_daily_working_hours",
+		"fieldtype": "Float",
+	}
+	if not frappe.db.exists("Custom Field", custom_field_filters):
 		return
 
-	for employee_id in frappe.get_all("Employee", filters={"expected_daily_working_hours": ("is", "set")}, pluck="name")::
+	for employee_id in frappe.get_all(
+		"Employee", filters={"expected_daily_working_hours": ("is", "set")}, pluck="name"
+	):
 		print(f"Updating {employee_id}")
 
 		employee = frappe.get_doc("Employee", employee_id)
@@ -22,6 +27,4 @@ def execute():
 			},
 		)
 		employee.save()
-	frappe.db.delete(
-		"Custom Field", {"dt": "Employee", "fieldname": "expected_daily_working_hours", "fieldtype": "Float"}
-	)
+	frappe.db.delete("Custom Field", custom_field_filters)
