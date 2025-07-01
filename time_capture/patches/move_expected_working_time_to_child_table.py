871618a24f5dbb57f298a5b6db2109fffc66f378
Patrick Eißler <77415730+PatrickDEissler@users.noreply.github.com>
2025-07-01 14:22:29 +0200

    refactor: general refactoring in patch
    Co-authored-by: Raffael Meyer <14891507+barredterra@users.noreply.github.com>

--- time_capture/patches/move_expected_working_time_to_child_table.py
+++ time_capture/patches/move_expected_working_time_to_child_table.py
@@ -6,9 +6,14 @@ def execute():
 		"Custom Field", {"dt": "Employee", "fieldname": "expected_daily_working_hours", "fieldtype": "Float"}
 	):
 		return
-	for e in frappe.db.get_all("Employee", filters={"expected_daily_working_hours": ["is", "set"]}):
-		print(f"Updating {e.name}")
-		employee = frappe.get_doc("Employee", e.name)
+
+	for employee_id in frappe.get_all("Employee", filters={"expected_daily_working_hours": ("is", "set")}, pluck="name")::
+		print(f"Updating {employee_id}")
+
+		employee = frappe.get_doc("Employee", employee_id)
+		if employee.expected_working_hours or not employee.expected_daily_working_hours:
+			continue
+
 		employee.append(
 			"expected_working_hours",
 			{
