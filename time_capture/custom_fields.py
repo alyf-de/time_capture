9fd3d620b765edd52971e198fc913ccd34061ca5
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2025-07-08 12:12:32 +0200

    feat: custom field in (and link to) Timesheet

--- time_capture/custom_fields.py
+++ time_capture/custom_fields.py
@@ -58,5 +58,13 @@ def get_custom_fields():
 				"options": "Time Capture",
 				"read_only": 1,
 			},
+			{
+			"label": _("Freelancer Time Capture"),
+			"fieldname": "freelancer_time_capture",
+			"insert_after": "custom_time_capture",
+			"fieldtype": "Link",
+			"options": "Freelancer Time Capture",
+			"read_only": 1,
+			}
 		],
 	}
