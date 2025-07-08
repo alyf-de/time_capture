5a986b280aa7c10c5eb7ee7fde77c9b581206d5b
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2025-07-08 10:25:56 +0200

    fix: make Patches for Property Setters + Custom Fields executable

--- time_capture/install.py
+++ time_capture/install.py
@@ -1,13 +1,17 @@
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
-
-from time_capture.patches.set_mandatory_breaks import set_mandatory_breaks
-
 from .custom_fields import get_custom_fields
 from .property_setters import get_property_setters
 from .utils import make_property_setters
+from time_capture.patches.set_mandatory_breaks import set_mandatory_breaks
 
 
 def after_install():
-	create_custom_fields(get_custom_fields())
-	make_property_setters(get_property_setters())
+	_make_custom_fields()
+	_make_property_setters()
 	set_mandatory_breaks()
+
+def _make_custom_fields():
+	create_custom_fields(get_custom_fields())
+
+def _make_property_setters():
+	make_property_setters(get_property_setters())
