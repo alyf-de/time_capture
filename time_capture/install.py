e95f1e653270436a732caea6df8e052953f8e645
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2025-07-08 10:38:03 +0200

    refactor: general refactor in install.py

--- time_capture/install.py
+++ time_capture/install.py
@@ -1,8 +1,10 @@
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+
+from time_capture.patches.set_mandatory_breaks import set_mandatory_breaks
+
 from .custom_fields import get_custom_fields
 from .property_setters import get_property_setters
 from .utils import make_property_setters
-from time_capture.patches.set_mandatory_breaks import set_mandatory_breaks
 
 
 def after_install():
@@ -10,8 +12,10 @@ def after_install():
 	_make_property_setters()
 	set_mandatory_breaks()
 
+
 def _make_custom_fields():
 	create_custom_fields(get_custom_fields())
+
 
 def _make_property_setters():
 	make_property_setters(get_property_setters())
