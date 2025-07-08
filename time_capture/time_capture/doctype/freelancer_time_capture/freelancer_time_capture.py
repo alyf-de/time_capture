a79df8def227743ccda74796ecf2037830dab2df
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2025-07-08 12:21:10 +0200

    feat: validate that self.user is a Freelancer

--- time_capture/time_capture/doctype/freelancer_time_capture/freelancer_time_capture.py
+++ time_capture/time_capture/doctype/freelancer_time_capture/freelancer_time_capture.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2025, ALYF GmbH and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
 
 class FreelancerTimeCapture(Document):
-	pass
+	def before_validate(self):
+		self.validate_user()
+
+	def validate_user(self):
+		if not "Freelancer" in frappe.get_roles(self.user):
+			frappe.throw(
+				_("Freelancer Time Capture can only be created for users with the 'Freelancer' role. Change the user in the field 'User'."),
+				title=_("User is not a Freelancer"),
+			)
