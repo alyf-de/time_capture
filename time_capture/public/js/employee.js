68773f07eaff72e989970adf1e7e187ca5d916d7
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2025-09-10 20:32:54 +0200

    feat: allocate weekly_working_hours for Mo to Fr if set directly

--- time_capture/public/js/employee.js
+++ time_capture/public/js/employee.js
@@ -8,3 +8,24 @@ frappe.ui.form.on("Employee", {
 		);
 	},
 });
+
+frappe.ui.form.on("Employee Expected Working Hours", {
+	weekly_working_hours: function (frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		if (row.weekly_working_hours) {
+			let daily_hours = row.weekly_working_hours / 5;
+			frappe.model.set_value(cdt, cdn, "monday", daily_hours);
+			frappe.model.set_value(cdt, cdn, "tuesday", daily_hours);
+			frappe.model.set_value(cdt, cdn, "wednesday", daily_hours);
+			frappe.model.set_value(cdt, cdn, "thursday", daily_hours);
+			frappe.model.set_value(cdt, cdn, "friday", daily_hours);
+
+			frappe.show_alert({
+				message: __("Updated Monday to Friday to {0} h each", [
+					format_number(daily_hours),
+				]),
+				indicator: "green",
+			});
+		}
+	},
+});
