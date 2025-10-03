8cd235ec6ad71313ed8eb4fd3982721f394fb27e
Patrick Eißler <77415730+PatrickDEissler@users.noreply.github.com>
2025-10-03 19:47:05 +0200

    fix: repair & simplify report "Working TIme Summary" (re-use new utils functions) (#35)

--- time_capture/time_capture/report/working_time_summary/working_time_summary.py
+++ time_capture/time_capture/report/working_time_summary/working_time_summary.py
@@ -2,11 +2,10 @@
 # For license information, please see license.txt
 # Source: https://github.com/alyf-de/arbeitszeiterfassung_s4a/tree/develop/arbeitszeiterfassung_s4a/arbeitszeiterfassung_s4a/report/working_time
 
-from datetime import date
-
 import frappe
 from frappe import _
-from frappe.utils import flt
+
+from time_capture.scripts.summary_utils import get_working_time_summary_for_employee
 
 
 def execute(filters=None):
@@ -18,21 +17,15 @@ def get_data(filters: dict):
 	employee_data = get_employees(filters)
 
 	for row in employee_data:
-		correction_hours, correction_date = get_flexitime_correction(row.employee)
-		attendance_data = get_hours_from_attendance(row.employee, correction_date)
+		# Get summary data using the summary_utils function
+		summary_data = get_working_time_summary_for_employee(row.employee, beautified=True)
 
-		actual = attendance_data.get("actual_working_hours") or 0
-		expected = attendance_data.get("expected_working_hours") or 0
-		balance = flt(actual - expected + (correction_hours or 0))
 		working_time_summary.append(
 			{
 				"employee": row.employee,
 				"employee_name": row.employee_name,
-				"last_correction": correction_date,
-				"correction_value": correction_hours,
-				"expected_working_hours": expected,
-				"actual_working_hours": actual,
-				"balance": balance,
+				"flexitime_correction": summary_data.get("flexitime_correction", "-"),
+				"current_balance": summary_data.get("current_balance", "0:00 h"),
 			}
 		)
 
@@ -53,41 +46,6 @@ def get_employees(filters: dict):
 	)
 
 
-def get_hours_from_attendance(employee: str, from_date: str | None = None) -> list[dict]:
-	"""Return the total hours from attendance records."""
-	filters = [
-		["employee", "=", employee],
-		["docstatus", "=", 1],
-	]
-	if from_date:
-		filters.append(["attendance_date", ">=", from_date])
-
-	attendance_hours = frappe.get_list(
-		"Attendance",
-		fields=[
-			"SUM(expected_working_hours) as expected_working_hours",
-			"SUM(working_hours) as actual_working_hours",
-		],
-		filters=filters,
-	)
-
-	return attendance_hours[0] if attendance_hours else {}
-
-
-def get_flexitime_correction(employee: str) -> "tuple[float, date]":
-	"""Return the last flexitime correction for the given employee."""
-	filters = {"employee": employee, "docstatus": 1}
-	if frappe.db.exists("Flexitime Correction", filters):
-		return frappe.db.get_value(
-			"Flexitime Correction",
-			filters=filters,
-			fieldname=["flexitime_hours", "date"],
-			order_by="date DESC",
-		)
-
-	return (None, None)
-
-
 def get_columns() -> list[dict]:
 	return [
 		{
@@ -100,30 +58,17 @@ def get_columns() -> list[dict]:
 			"fieldname": "employee_name",
 			"fieldtype": "Data",
 			"label": _("Employee Name"),
+			"width": 300,
 		},
 		{
-			"fieldname": "last_correction",
-			"fieldtype": "Date",
-			"label": _("Last Manual Correction"),
+			"fieldname": "flexitime_correction",
+			"fieldtype": "Data",
+			"label": _("Flexitime Correction"),
+			"width": 300,
 		},
 		{
-			"fieldname": "correction_value",
-			"fieldtype": "Float",
-			"label": _("Correction Value"),
-		},
-		{
-			"fieldname": "expected_working_hours",
-			"fieldtype": "Float",
-			"label": _("Expected Working Hours"),
-		},
-		{
-			"fieldname": "actual_working_hours",
-			"fieldtype": "Float",
-			"label": _("Actual Working Hours"),
-		},
-		{
-			"fieldname": "balance",
-			"fieldtype": "Float",
-			"label": _("Balance"),
+			"fieldname": "current_balance",
+			"fieldtype": "Data",
+			"label": _("Current Balance"),
 		},
 	]
