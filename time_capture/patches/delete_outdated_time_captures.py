648e92cc1e18f15f77caa38e0f1a81072fe66f60
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2025-10-27 14:06:59 +0100

    fix: delete outdated time captures via patch

---
+++ time_capture/patches/delete_outdated_time_captures.py
@@ -0,0 +1,21 @@
+import frappe
+from time_capture.scripts.attendance import delete_time_capture
+
+
+def execute():
+	"""
+	Deletes Time Captures that are not needed anymore.
+	Reason: They have been outdated by leave days.
+	"""
+	leave_days = frappe.db.get_all(
+		"Attendance",
+		filters={
+			"docstatus": 1,
+			"custom_time_capture": ["!=", None],
+			"leave_type": ["!=", None],
+			"attendance_date": ("<=", frappe.utils.nowdate()),
+		},
+		pluck="name",
+	)
+	for attendance_name in leave_days:
+		delete_time_capture(frappe.get_doc("Attendance", attendance_name))
